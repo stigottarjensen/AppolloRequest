d82f9a0039f6c77da0cff9e1ff2ce6526fa35551
stigottarjensen <stig@nornir.io>
2024-01-04 21:53:04 +0100

    bubb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AppolloRequest';
+  jsonResult: any = 'empty';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const date = new Date();
+    date.getTime();
+    this.http
+      .get('http://localhost:8778/AbisDB2JSON/abis/hive', { withCredentials: true })
+      .subscribe((result: any) => {
+        console.log(result);
+        this.jsonResult = result;
+      });
+  }
 }
