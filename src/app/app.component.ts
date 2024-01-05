322991b95a4c3f9480b13c5e6d72c0134e3a8741
stigottarjensen <stig@nornir.io>
2024-01-05 13:33:47 +0100

    e4eeee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,22 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'AppolloRequest';
   jsonResult: any = 'empty';
+  filterTypes: string[] = ['none', 'equal', 'in', 'like', 'between'];
+  rtw: any = {
+    RTW: {
+      TIMESTAMP: '',
+      SENDER: 'The Stig',
+      RECEIVER: 'Chessmaster Levang',
+      TOPIC: 'queryrequest',
+      REFID: 'uuid',
+      PAYLOAD: {
+        request_name: '',
+        fields: [],
+        filters: [],
+        template: '',
+      },
+    },
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -17,10 +33,27 @@ export class AppComponent implements OnInit {
     const date = new Date();
     date.getTime();
     this.http
-      .get('http://localhost:8778/AbisDB2JSON/abis/hive', { withCredentials: true })
+      .get('http://localhost:8778/AbisDB2JSON/abis/hive', {
+        withCredentials: true,
+      })
       .subscribe((result: any) => {
-        console.log(result);
         this.jsonResult = result;
+        this.jsonResult.forEach((element: any) => {
+          element.open = false;
+          element.filter_template = '';
+          element.RTW = JSON.parse(JSON.stringify(this.rtw.RTW));
+          element.RTW.PAYLOAD.request_name = element.table_name;
+          element.fields.forEach((field: any) => {
+            field.checked = false;
+            field.selectedFilter = 'none';
+            field.filter_values = '';
+          });
+        });
+        console.log(this.jsonResult);
       });
+  }
+
+  anyChange(event: any) {
+    console.log(event);
   }
 }
